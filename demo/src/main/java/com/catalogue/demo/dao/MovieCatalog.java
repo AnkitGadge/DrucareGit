7fd21153cd3ca2c019b684949cce8eb044b59951
AnkitGadge <99271017+AnkitGadge@users.noreply.github.com>
2022-04-28 17:29:26 +0530

    deletedemo

--- demo/src/main/java/com/catalogue/demo/dao/MovieCatalog.java
+++
@@ -1,9 +0,0 @@
-package com.catalogue.demo.dao;
-
-import java.util.List;
-
-import com.catalogue.demo.Entity.CatalogItem;
-
-public interface MovieCatalog {
-	public List<CatalogItem> getCatalog(String userId);
-}
